c63ed16b173d9421fa7bd6a1984237309b18d8b0
Jacek Krużycki <kruzyk@gmail.com>
2020-09-26 12:10:46 +0200

    sets initial state

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 
+import './style.css';
 
+// import { ReactComponent as PacmanSvg } from './pacman.svg';
 
 class Pacman extends Component {
+
+    state = {
+        direction: 'right',
+        position: {
+            top: 50,
+            left: 0
+        }
+    }
+
     render() {
         return (
-            <div className="pacman">
+            <div className="pacman" style={this.state.position}>
                 Oo
                 {/* {PacmanSvg} */}
             </div>
         )
     }
+}
+
+Pacman.defaultProps = {
+    step: 50,
+    size: 50, // pacman size is 50x50px
+    //TODO: move to config
+    border: 10 * 2,
+    topScoreBoardHeight: 50
 }
 
 export default Pacman;

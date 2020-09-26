9897a27d6b83edd373b34ecaafe203b2c3cde904
Jacek Krużycki <kruzyk@gmail.com>
2020-09-26 13:25:51 +0200

    adds transformations for pacman

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -31,13 +31,15 @@ class Pacman extends Component {
     }
 
     render() {
+        const { direction, position } = this.state;
+
         return (
             <div
                 ref={this.pacmanRef}
-                className="pacman"
+                className={`pacman pacman--${direction}`}
                 tabIndex="0"
                 onKeyDown={this.handleKeyDown}
-                style={this.state.position}>
+                style={position}>
                 <PacmanSvg />
             </div>
         )
